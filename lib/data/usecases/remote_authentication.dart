c62146e862a64daef480f75610df4d3f2d12daea
Breno Augusto <breno.augustosm@gmail.com>
2020-12-23 18:18:47 -0300

    Move api details from domain class to data class.

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,3 +1,4 @@
+import 'package:for_dev/domain/entities/account_entity.dart';
 import 'package:meta/meta.dart';
 
 import '../../domain/usecases/usecases.dart';
@@ -17,7 +18,22 @@ class RemoteAuthentication {
     await httpClient.request(
       url: url,
       method: 'post',
-      body: params.toJson(),
+      body: RemoteAuthenticationParams.fromDomain(params).toJson(),
     );
   }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    @required this.email,
+    @required this.password,
+  });
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(email: params.email, password: params.secret);
+
+  Map toJson() => {'email': email, 'password': password};
 }
