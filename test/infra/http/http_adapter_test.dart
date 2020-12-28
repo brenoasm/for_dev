e556974aec7669bd1a11f16aaf31a17418396a49
Breno Augusto <breno.augustosm@gmail.com>
2020-12-28 10:15:27 -0300

    Ensure HttpAdapter calls post with correct body.

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
@@ -19,7 +21,11 @@ class HttpAdapter {
       'accept': 'application/json',
     };
 
-    await client.post(url, headers: headers);
+    await client.post(
+      url,
+      headers: headers,
+      body: jsonEncode(body),
+    );
   }
 }
 
@@ -38,7 +44,11 @@ main() {
 
   group('HttpAdapter | Post', () {
     test('should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut.request(
+        url: url,
+        method: 'post',
+        body: {'any_key': 'any_value'},
+      );
 
       verify(
         client.post(
@@ -47,6 +57,7 @@ main() {
             'content-type': 'application/json',
             'accept': 'application/json',
           },
+          body: '{"any_key":"any_value"}',
         ),
       );
     });
