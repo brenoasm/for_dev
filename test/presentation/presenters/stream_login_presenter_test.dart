6414513b20303ca676d39644093c8c0435babb82
Breno Augusto <breno.augustosm@gmail.com>
2020-12-30 09:59:19 -0300

    Ensure email EmailErrorStream will not emit duplicated event.

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -40,8 +40,13 @@ main() {
     test('should emit email error if validation fails', () async {
       mockValidation(value: 'error');
 
-      expectLater(sut.emailErrorStream, emits('error'));
+      sut.emailErrorStream.listen(
+        expectAsync1(
+          (error) => expect(error, 'error'),
+        ),
+      );
 
+      sut.validateEmail(email);
       sut.validateEmail(email);
     });
   });
