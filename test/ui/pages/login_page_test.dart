9f82ba58e1e3651b25b8fd11bf109601db39ea6d
Breno Augusto <breno.augustosm@gmail.com>
2021-01-16 20:00:21 -0300

    Ensure SplashPage navigate to correct page.

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -295,5 +295,17 @@ main() {
       expect(Get.currentRoute, '/any_route');
       expect(find.text('fake page'), findsOneWidget);
     });
+
+    testWidgets('should not change page', (WidgetTester tester) async {
+      await loadPage(tester);
+
+      navigateToController.add('');
+      await tester.pump();
+      expect(Get.currentRoute, '/login');
+
+      navigateToController.add(null);
+      await tester.pump();
+      expect(Get.currentRoute, '/login');
+    });
   });
 }
