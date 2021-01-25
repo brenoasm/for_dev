a7805242d0fed34ac4e7d2b1f3468f61f97c3edc
Breno Augusto <breno.augustosm@gmail.com>
2021-01-25 13:54:34 -0300

    Add AddAccount use case.

---
+++ lib/domain/usecases/add_account.dart
@@ -0,0 +1,30 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import '../entities/entities.dart';
+
+abstract class AddAccount {
+  Future<AccountEntity> add(AddAccountParams params);
+}
+
+class AddAccountParams extends Equatable {
+  final String email;
+  final String name;
+  final String password;
+  final String passwordConfirmation;
+
+  AddAccountParams({
+    @required this.email,
+    @required this.name,
+    @required this.password,
+    @required this.passwordConfirmation,
+  });
+
+  @override
+  List<Object> get props => [
+        email,
+        name,
+        password,
+        passwordConfirmation,
+      ];
+}
